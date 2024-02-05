eaab648d83b7fdc7c7cf9838059db82735d2b7c1
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2024-02-05 19:23:17 +0200

    Remove old jumbotron elements and implement hovering gradients circles

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -3,14 +3,13 @@ import './Jumbotron.css'
 export default function Jumbotron() {
     return (
         <>
-            <div className='jumbotron-container'>
-                <div className='jumbotron-copy'>
-                    <h1>Popa Andrei-Valentin</h1>
-                    <p>I develop <b>digital solutions</b> for various challenges.</p>
-                    <div className='frosted-glass' />
-                    <div className='dot'/>
-                    <div className='dot -second'/>
-                    <div className='dot -third'/>
+            <div className='gradient-bg'>
+                <div className='gradients-container'>
+                    <div className='g1'></div>
+                    <div className='g2'></div>
+                    <div className='g3'></div>
+                    <div className='g4'></div>
+                    <div className='g5'></div>
                 </div>
             </div>
         </>
