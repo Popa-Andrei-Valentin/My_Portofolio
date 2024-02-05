a799bcf089c516ea70d991158ff8c7e290e4dd87
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2024-02-05 22:07:39 +0200

    Organise the css for the entire app

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Jumbotron from "./components/Jumbotron/Jumbotron.jsx";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
         <div className="root-content">
