33dfe74060ddf26c6403c7cfdfc9e91687cb0c36
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2024-02-06 19:32:13 +0200

    Fix useEffect maximum depth error

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -1,30 +1,19 @@
 import './Jumbotron.css'
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 
 export default function Jumbotron() {
+    const mouseBubble = useRef(null);
     const [curX, setCurX] = useState(0);
     const [curY, setCurY] = useState(0);
-    const [tgX, setTgX] = useState(0);
-    const [tgY, setTgY] = useState(0);
-
-    let moveGradient = (element) => {
-        setCurX(currentX => currentX + (tgX - currentX) / 20);
-        setCurY(curY => curY + (tgY - curY) / 20);
-        element.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`
-        requestAnimationFrame(() => {
-            moveGradient();
-        })
-    }
 
     useEffect(() => {
-        const interBubble = document.querySelector('.interactive');
-
+        /** Update gradient position in order to be under the cursor */
         window.addEventListener('mousemove', (event) => {
-            setTgX(event.clientX);
-            setTgY(event.clientY);
+            setCurX(() => event.clientX);
+            setCurY(() => event.clientY);
+            mouseBubble.current.style.left = `${curX}px`
+            mouseBubble.current.style.top = `${curY}px`
         });
-
-        moveGradient(interBubble);
     })
 
     return (
@@ -50,7 +39,7 @@ export default function Jumbotron() {
                         <div className='g3'></div>
                         <div className='g4'></div>
                         <div className='g5'></div>
-                        <div className='interactive'></div>
+                        <div ref={mouseBubble} className='interactive'></div>
                     </div>
                 </div>
             </div>
