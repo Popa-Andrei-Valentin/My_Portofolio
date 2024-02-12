2e2751a8f1e52d5de0c01e80636d58917ac1011e
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2024-02-12 19:37:11 +0200

    Implement ToggleBtn in App.jsx and modify some inline stuff for svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
 import Jumbotron from "./components/Jumbotron/Jumbotron.jsx";
 import './App.css'
+import ToggleBtn from "./components/ToggleBtn/ToggleBtn.jsx";
 
 function App() {
   return (
     <>
         <div className="root-content">
+            <ToggleBtn />
             <Jumbotron />
         </div>
     </>
