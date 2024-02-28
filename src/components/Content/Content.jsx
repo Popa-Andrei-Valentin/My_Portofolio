52d60804b47546cbb05a052ecb32f572d2a4d784
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2024-02-28 19:31:33 +0200

    Add placholder divs in Content.js

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -4,8 +4,16 @@ export default function Content () {
 	return (
 		<>
 			<div className='content-container'>
-				<h1>About me</h1>
-				<div>
+				<div className='content-text'>
+					<h2>About me</h2>
+					<p>For placeholder text on websites, the most commonly used Latin text is "Lorem ipsum," which is a scrambled segment from a piece by Cicero. However, if you're looking for different Latin quotes that could serve as interesting, more unique placeholder text, here are a few classical Latin quotes along with their translations:</p>
+				</div>
+				<div className='content-text'>
+					<h2>Experience</h2>
+					<p>For placeholder text on websites, the most commonly used Latin text is "Lorem ipsum," which is a scrambled segment from a piece by Cicero. However, if you're looking for different Latin quotes that could serve as interesting, more unique placeholder text, here are a few classical Latin quotes along with their translations:</p>
+				</div>
+				<div className='content-text'>
+					<h2>My projects</h2>
 					<p>For placeholder text on websites, the most commonly used Latin text is "Lorem ipsum," which is a scrambled segment from a piece by Cicero. However, if you're looking for different Latin quotes that could serve as interesting, more unique placeholder text, here are a few classical Latin quotes along with their translations:</p>
 				</div>
 			</div>
