58100d5f0d3dc0354d52feed518162eb50cd920d
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2024-03-12 09:36:23 +0200

    Create experience cards and write description for each job

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -9,6 +9,22 @@ export default function Content () {
 						<h2>Experience</h2>
 						<div className='content-box'>
 							<p>Working as a software developer with a focus on the frontend side for <b>two years</b> has been a transformative journey, blending creativity with technical skills. I've navigated through an ever-evolving landscape of web technologies, honing abilities in various modern frameworks such as <b>React</b> and <b>Vue.js</b>.</p>
+							<div className='experience-container'>
+								<div className='experience-title'>
+									<header>December 2023 - PRESENT</header>
+									<span>Frontend Developer - Arnia Software</span>
+								</div>
+								<p>Style, build and ship high-quality web applications for a diverse array of projects for clients of Arnia Software. Currently working for kfzteile24 team, collaborating with international colleagues, including a skilled team of backend and infrastructure developers to enhance the e-commerce platform.
+								</p>
+							</div>
+							<div className='experience-container'>
+								<div className='experience-title'>
+									<header>May 2022 - December 2023</header>
+									<span>Frontend Developer - Cegedim</span>
+								</div>
+								<p>Built and maintained critical components used to construct MyTeamsRH frontend across the whole platform. Migrated the code base from Vue 2 to Vue 3, including adaptations for the new versions of Vuex Store and Vue Router.
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
