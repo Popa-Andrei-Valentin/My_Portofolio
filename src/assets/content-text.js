0918103dfbd2fb2b63707ce72015089105f312fd
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2024-03-12 22:35:54 +0200

    Update style vars

--- src/assets/content-text.js
+++ src/assets/content-text.js
@@ -1,11 +1,11 @@
 export const contentText = [
 	{
-		date:'December 2023 - PRESENT',
+		date:'DEC 2023 - PRESENT',
 		title:'Frontend Developer - Arnia Software',
 		description:'Style, build and ship high-quality web applications for a diverse array of projects for clients of Arnia Software. Currently working for kfzteile24 team, collaborating with international colleagues, including a skilled team of backend and infrastructure developers to enhance the e-commerce platform.'
 	},
 	{
-		date:'May 2022 - December 2023',
+		date:'MAY 2022 - DEC 2023',
 		title:'Frontend Developer - Cegedim',
 		description:'Built and maintained critical components used to construct MyTeamsRH frontend across the whole platform. Migrated the code base from Vue 2 to Vue 3, including adaptations for the new versions of Vuex Store and Vue Router.',
 	}
