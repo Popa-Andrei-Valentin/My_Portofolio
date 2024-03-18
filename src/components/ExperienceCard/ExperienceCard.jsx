ccc9a2e66b2cf428ca4eb2a3560cf2991c38cd35
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2024-03-18 21:30:07 +0200

    Add arrow icon

--- src/components/ExperienceCard/ExperienceCard.jsx
+++ src/components/ExperienceCard/ExperienceCard.jsx
@@ -1,5 +1,6 @@
 import './ExperienceCard.css'
-import {useState} from "react";
+import {useState} from 'react';
+import ArrowUp from "../Icons/arrowUp.jsx";
 
 export default function ExperienceCard ({date, title, description, hoverEvent, isHovered}) {
 	const [selected, setSelected] = useState(false);
@@ -17,6 +18,7 @@ export default function ExperienceCard ({date, title, description, hoverEvent, i
 				<div className='experience-title'>
 					<header>{date}</header>
 					<span>{title}</span>
+					<ArrowUp />
 				</div>
 				<p>
 					{description}
---
+++ src/components/Icons/arrowUp.jsx
@@ -0,0 +1,7 @@
+export default function ArrowUp() {
+	return(
+		<>
+			<svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="24" height="24"><path d="M22,0H10V2h10.586L.043,22.543l1.414,1.414L22,3.414V14h2V2c0-1.103-.897-2-2-2Z"/></svg>
+		</>
+	)
+}
