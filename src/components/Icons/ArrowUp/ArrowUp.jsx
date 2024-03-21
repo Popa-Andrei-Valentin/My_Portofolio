7f4f4ca4d96286382b81dadcc4160082195010c2
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2024-03-21 22:40:16 +0200

    Add stroke-width for svg

--- src/components/Icons/ArrowUp/ArrowUp.jsx
+++ src/components/Icons/ArrowUp/ArrowUp.jsx
@@ -2,7 +2,18 @@ import './ArrowUp.css'
 export default function ArrowUp() {
 	return(
 		<>
-			<svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="24" height="24"><path d="M22,0H10V2h10.586L.043,22.543l1.414,1.414L22,3.414V14h2V2c0-1.103-.897-2-2-2Z"/></svg>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				id="Layer_1"
+				data-name="Layer 1"
+				viewBox="0 0 24 24"
+				width="24"
+				height="24"
+			>
+				<path stroke="white" stroke-width="3"
+					d="M22,0H10V2h10.586L.043,22.543l1.414,1.414L22,3.414V14h2V2c0-1.103-.897-2-2-2Z"
+				/>
+			</svg>
 		</>
 	)
 }
