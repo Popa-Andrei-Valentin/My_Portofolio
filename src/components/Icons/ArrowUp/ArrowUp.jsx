543dd7400954442428bc05d1f22bff3b2e3f8b4c
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2024-03-27 22:22:06 +0200

    Update arrow styling

--- src/components/Icons/ArrowUp/ArrowUp.jsx
+++ src/components/Icons/ArrowUp/ArrowUp.jsx
@@ -7,10 +7,10 @@ export default function ArrowUp() {
 				id="Layer_1"
 				data-name="Layer 1"
 				viewBox="0 0 24 24"
-				width="24"
-				height="24"
+				width="10"
+				height="10"
 			>
-				<path stroke="white" stroke-width="3"
+				<path stroke="white" stroke-width="1"
 					d="M22,0H10V2h10.586L.043,22.543l1.414,1.414L22,3.414V14h2V2c0-1.103-.897-2-2-2Z"
 				/>
 			</svg>
