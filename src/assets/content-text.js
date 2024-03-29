5d219ee2cb3364ea88efb86fde246e234a6e25d0
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2024-03-29 19:10:33 +0200

    Add tech-stack array in content-text

--- src/assets/content-text.js
+++ src/assets/content-text.js
@@ -3,12 +3,14 @@ export const contentText = [
 		linkToOpen: 'https://www.arnia.com/',
 		date:'DEC 2023 - PRESENT',
 		title:'Frontend Developer - Arnia Software',
-		description:'Style, build and ship high-quality web applications for a diverse array of projects for clients of Arnia Software. Currently working for kfzteile24 team, collaborating with international colleagues, including a skilled team of backend and infrastructure developers to enhance the e-commerce platform.'
+		description:'Style, build and ship high-quality web applications for a diverse array of projects for clients of Arnia Software. Currently working for kfzteile24 team, collaborating with international colleagues, including a skilled team of backend and infrastructure developers to enhance the e-commerce platform.',
+		techStack: ['Javascript','Vue 3','Vue SSR','React','Twig','Storybook','HTML & SCSS']
 	},
 	{
 		linkToOpen: 'https://www.cegedim.com/Pages/default.aspx',
 		date:'MAY 2022 - DEC 2023',
 		title:'Frontend Developer - Cegedim',
 		description:'Built and maintained critical components used to construct MyTeamsRH frontend across the whole platform. Migrated the code base from Vue 2 to Vue 3, including adaptations for the new versions of Vuex Store and Vue Router.',
+		techStack: ['Javascript','HTML & CSS', 'Vue 2','Vue 3']
 	}
 ]
