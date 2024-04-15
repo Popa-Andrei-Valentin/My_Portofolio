48c33257b0c9b6e5331ebe8ea96ba3b6114a694d
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2024-04-15 21:29:17 +0300

    Remove gradient balls, in order to improve efficiency

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -25,10 +25,10 @@ export default function Jumbotron() {
                     <div className='gradients-container'>
                         <div className='g1'></div>
                         <div className='g2'></div>
-                        <div className='g3'></div>
-                        <div className='g4'></div>
-                        <div className='g5'></div>
-                        <div className='interactive'></div>
+                        {/*<div className='g3'></div>*/}
+                        {/*<div className='g4'></div>*/}
+                        {/*<div className='g5'></div>*/}
+                        {/*<div className='interactive'></div>*/}
                     </div>
                 </div>
             </div>
