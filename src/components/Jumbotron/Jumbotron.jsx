44f723151e25cfb8a2627507a34f15e87f8f02d1
Popa-Andrei-Arnia <andrei_popa2009@icloud.com>
2024-04-27 22:27:34 +0300

    Implement CV downloading functionality and fix code spacing

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -1,4 +1,5 @@
 import './Jumbotron.css'
+import CV from '../../assets/CV.pdf'
 
 export default function Jumbotron() {
     return (
@@ -7,25 +8,27 @@ export default function Jumbotron() {
                 <div className='jumbotron-text'>
                     <h1>Solution infrastructure for the internet</h1>
                     <h2>Hi, I'm Andrei.</h2>
-										<div className='button-container'>
-											<button className='btn-standard'>Contact me</button>
-											<button className='btn-standard btn-cv'>CV</button>
-										</div>
+                    <div className='button-container'>
+                        <button className='btn-standard'>Contact me</button>
+                    <a href={CV} download='CV'>
+                        <button className='btn-standard btn-cv'>CV</button>
+                    </a>
                 </div>
-                <div className='gradient-bg'>
-                        <svg xmlns="http://www.w3.org/2000/svg">
-                            <defs>
-                                <filter id="goo">
-                                    <feColorMatrix
-                                        in="blur"
-                                        mode="matrix"
-                                        values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
-                                        result="goo"
-                                    />
-                                    <animate attributeName="values" from="0" to="360" dur="20s" repeatCount="indefinite"/>
-                                </filter>
-                            </defs>
-                        </svg>
+            </div>
+            <div className='gradient-bg'>
+                    <svg xmlns="http://www.w3.org/2000/svg">
+                        <defs>
+                            <filter id="goo">
+                                <feColorMatrix
+                                    in="blur"
+                                    mode="matrix"
+                                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                                    result="goo"
+                                />
+                                <animate attributeName="values" from="0" to="360" dur="20s" repeatCount="indefinite"/>
+                            </filter>
+                        </defs>
+                    </svg>
                     <div className='gradients-container'>
                         <div className='g1'></div>
                         <div className='g2'></div>
